3b131258ed68aca21c01ea3d765cbc568c26afe8
NbekWeb <nbekdev@gmail.com>
2025-04-09 04:35:51 +0500

    fix

--- src/app/dashboard/start/page.tsx
+++ src/app/dashboard/start/page.tsx
@@ -1,15 +1,31 @@
 'use client'
 import Image from "next/image";
 import { Button, Spin } from 'antd';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { Navigation, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import Arrow from "@/components/icons/arrow";
-
+import SwiperCore from "swiper";
 
 export default function Start() {
     const [filteredData, setFilteredData] = useState([1, 2, 3]);
+    const [last, setLast] = useState(false);
+    const swiperRef = useRef<SwiperCore>(null);
+    const router = useRouter();
+
+    const handleNextClick = () => {
+        if (!swiperRef.current) return;
+
+        const swiper = swiperRef.current;
+        if (swiper.isEnd && !last) {
+            setLast(true)
+        }
+    };
+
+    const goNext = () => {
+        router.push('/upload');
+    }
 
     return (
         <div>
@@ -25,7 +41,9 @@ export default function Start() {
                     dynamicMainBullets: 2,
                 }}
                 loop={false}
-
+                onSwiper={(swiper) => {
+                    swiperRef.current = swiper;
+                }}
                 slidesPerView={1}
                 spaceBetween={10}
             >
@@ -48,12 +66,19 @@ export default function Start() {
                     Unlock the Power
                     Of  Future AI
                 </h2>
-                <p className="text-white-700 font-light text-sm mt-1 mb-7">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do </p>
+                <p className="text-gray-700 font-light text-sm mt-1 mb-7">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do </p>
             </div>
             <div className="flex max-w-max text-xl text-dark-200 mx-auto shadow-btn gap-7 p-4.5 rounded-xl items-center">
                 <Arrow className="rotate-180 custom-prev-new text-gray-200" />
                 <span className="bg-gray-300 h-5 w-0.5 flex min-w-0.5"></span>
-                <Arrow className="custom-next-new" />
+
+                <button className={!last ? 'custom-next-new' : 'hidden'} onClick={handleNextClick} >
+                    <Arrow />
+                </button>
+                <button className={last ? '' : 'hidden'} onClick={goNext} >
+                    <Arrow />
+                </button>
+
             </div>
         </div>
     );
